6a1d9c245d663c31a96d3ff442250a3dbd9637fb
GAA251 <135707795+GAA251@users.noreply.github.com>
2026-01-19 20:31:53 +0300

    Update ConfigureVAAPI.sh

--- files/scripts/ConfigureVAAPI.sh
+++ files/scripts/ConfigureVAAPI.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# Enable UUPD (Universal Update)
-# Normally this is supposed to be provided by bling (https://blue-build.org/reference/modules/bling/)
-# but it isn't (for now). So we install it through recipe.yml, then configure it here.
-
-# For this script to work; 
-# ublue-os-update-services must be removed and uupd must be installed through recipe.yml
+# WARNING!
+# ujust configure-nvidia still needs to be run to configure the NVIDIA drivers and to enable the firefox flatpak to access the libva driver
 
 # Tell this script to exit if there are any errors.
 set -oue pipefail
+
 echo "LIBVA_DRIVER_NAME=nvidia;CUDA_DISABLE_PERF_BOOST=1" >> /etc/environment
