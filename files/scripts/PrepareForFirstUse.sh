b37dff6834aacc1af81c4454035f5f80a4f6e13b
GA251 <135707795+GA251@users.noreply.github.com>
2026-01-22 12:53:21 +0300

    Update PrepareForFirstUse.sh

--- files/scripts/PrepareForFirstUse.sh
+++ files/scripts/PrepareForFirstUse.sh
@@ -8,3 +8,9 @@ rm -f /etc/profile.d/toolbox.sh
 
 # Enables font stemming (https://blog.aktsbot.in/no-more-blurry-fonts.html)
 echo 'FREETYPE_PROPERTIES="cff:no-stem-darkening=0 autofitter:no-stem-darkening=0"' >> /etc/environment
+
+# TEMP!!!!
+# PLEASE REMOVE THIS AFTER THIS IS FIXED:
+# https://gitlab.gnome.org/GNOME/libgnome-volume-control/-/issues/34
+# Downgrades wireplumber to v0.5.12-1 until gnome stops crashing the second it switches to a bluetooth device
+dnf downgrade wireplumber-0.5.12-1.fc43
