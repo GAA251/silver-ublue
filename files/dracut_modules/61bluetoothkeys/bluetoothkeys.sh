35e80e80240132fc29287eb679ce056eb9f5c7f6
GA251 <135707795+GA251@users.noreply.github.com>
2026-02-02 20:48:19 +0300

    Update bluetoothkeys.sh

--- files/dracut_modules/61bluetoothkeys/bluetoothkeys.sh
+++ files/dracut_modules/61bluetoothkeys/bluetoothkeys.sh
@@ -2,5 +2,8 @@
 #/boot/_bluetooth/[bluetooth dongle mac]/[bluetooth keyboard mac]/info
 #don't want initramfs to choke if the keys arent copied yet
 if [ -d "/boot/_bluetooth" ]; then
+  warn "Bluetooth keys detected"
   cp -R /boot/_bluetooth/* /var/lib/bluetooth/
+  warn "Copied but to test I'm dropping you to the emergency shell. Things will continue as soon as you leave"
+  emergency_shell
 fi
