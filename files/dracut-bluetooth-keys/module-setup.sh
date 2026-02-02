64971766ae03992e46a6885af6dc7b161bd42c31
GA251 <135707795+GA251@users.noreply.github.com>
2026-02-02 19:38:18 +0300

    Update module-setup.sh

--- files/dracut-bluetooth-keys/module-setup.sh
+++ files/dracut-bluetooth-keys/module-setup.sh
@@ -5,6 +5,9 @@ check() {
 }
 
 depends() {
+  # This module obviously requires the bluetooth module
+  # the bluetooth module also has its own requirements but dracut loads them anyway
+  echo bluetooth
   return 0
 }
 
