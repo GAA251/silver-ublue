f09bccefa919f740fffd1965e77254df2f9f1b16
GA251 <135707795+GA251@users.noreply.github.com>
2026-02-04 22:21:46 +0300

    Update PrepareForFirstUse.sh to support Bluetooth

--- files/scripts/PrepareForFirstUse.sh
+++ files/scripts/PrepareForFirstUse.sh
@@ -9,6 +9,12 @@ rm -f /etc/profile.d/toolbox.sh
 # Enables font stemming (https://blog.aktsbot.in/no-more-blurry-fonts.html)
 echo 'FREETYPE_PROPERTIES="cff:no-stem-darkening=0 autofitter:no-stem-darkening=0"' >> /etc/environment
 
+# Configure bluetooth
+# Enables Fast Connectable, lets devices connect faster to us at startup/after suspend (/etc/bluetooth.main.conf)
+# Enables the bluetooth adapter to wake the computer from suspend (https://wiki.archlinux.org/title/Bluetooth#Wake_from_suspend)
+sed -i 's/#FastConnectable = false/FastConnectable = true/' /etc/bluetooth/main.conf
+echo 'ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTR{bDeviceClass}=="e0", ATTR{bDeviceProtocol}=="01", ATTR{bDeviceSubClass}=="01", ATTR{power/wakeup}="enabled"' > /etc/udev/rules.d/91-bluetooth-wakeup.rules
+
 # DUMMY FIX for anything weird that might happen in the future
 #current_date=$(date +%Y%m%d)
 #cutoff_date="20260206" # 06.02.2026 (DD/MM/YYYY)
